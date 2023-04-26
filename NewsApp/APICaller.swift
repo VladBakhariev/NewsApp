61e155ce46526d8eaf52879ef5ff222f4ece1308
Vlad <Vlad Bakh>
2023-04-26 20:56:27 +0300

    Define Codable structs for parsing API response"

--- NewsApp/APICaller.swift
+++ NewsApp/APICaller.swift
@@ -39,3 +39,21 @@ final class APICaller {
         task.resume()
     }
 }
+
+// Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String
+    let url: String
+    let urlToImage: String
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
