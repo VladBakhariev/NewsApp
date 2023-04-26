e95e3d6a66ef16776e60e06eccd092b5d2dde7a9
Vlad <Vlad Bakh>
2023-04-26 21:27:43 +0300

    Implement didSelectRowAt method to deselect the selected table view row

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -55,5 +55,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = "Something"
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
