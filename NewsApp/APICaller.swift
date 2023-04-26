18c7be23001b45e066185d67a67c0af3ae64c2b4
Vlad <Vlad Bakh>
2023-04-26 20:33:49 +0300

    Define a function to fetch top news stories from the news API and decode JSON response using URLSession and JSONDecoder

--- NewsApp/APICaller.swift
+++ NewsApp/APICaller.swift
@@ -15,7 +15,25 @@ final class APICaller {
                                             "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=API_KEY")
     }
     
-    private init() {
-        
+    private init() {}
+    
+    public func getTopStories(completion: @escaping (Result<[String], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                
+                do {
+                    let result = try JSONDecoder().decode(String.self, from: data)
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
     }
 }
